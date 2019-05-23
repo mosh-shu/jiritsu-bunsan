a3ee05a28121bc275a14d8ed475584fa639459b3
Shu Sakamoto <shusakamoto1707@gmail.com>
2019-05-23 16:51:26 +0900

    ready to submit hw

--- scripts/dsearch_hw.py
+++ scripts/dsearch_hw.py
@@ -20,12 +20,11 @@ def proc(id, card, n, resultq):
     find = 0
     logging.debug('start')
     # 探すべきnがcard[]内にあるかをチェック
-    for i in range(id*10, (id*10+10)):
+    for i in range(10):
         if (card[i] == n):
             logging.debug(i)
             logging.debug('find!!!')
-            find = 1
-            break
+            find += 1
     resultq.put(find)
     logging.debug('endloop')
 
@@ -41,6 +40,14 @@ if __name__ == '__main__':
     print("card all 0 clear:", card)
     for i in range(100):
         card[i] = random.randint(1, 1000)
+    #
+    # use this section to test multiple cards
+    #
+    # for i in range(5):
+    #     card[i] = 100
+    # card[32] = 100
+    # card[91] = 100
+
     print("card=", card)
 
     #
@@ -55,16 +62,17 @@ if __name__ == '__main__':
     threadlist = list()
     resultq = queue.Queue()
     for i in range(10):
-        t = threading.Thread(target=proc, args=(i, card, num, resultq, ))
+        card_t = card[i*10:i*10+10]
+        t = threading.Thread(target=proc, args=(i, card_t, num, resultq, ))
         threadlist.append(t)
         t.start()
     #
     #　スレッドの終了を待ち、結果を印刷
     #
     print(threadlist)
+    findnum = 0
     for thread in threadlist:
         thread.join()
-        if (resultq.get() == 1):
-            print("num=", num, "was found")
-            break
+        findnum += resultq.get()
+    print("%d cards of num = %d was found" % (findnum, num))
     print("All thread is ended.")
